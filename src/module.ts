5d41373dff60e53a132219807c8d8a56b24d3aad
Alyssa Bull <58453566+alyssabull@users.noreply.github.com>
2023-04-26 07:43:58 -0600

    Add initial feature tracking (#60)

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,34 @@
-import { DataSourcePlugin } from '@grafana/data';
+import { DashboardLoadedEvent, DataSourcePlugin } from '@grafana/data';
+import { getAppEvents } from '@grafana/runtime';
+import { analyzeQueries, trackSentryDashboardLoaded } from 'tracking';
 import { SentryDataSource } from './datasource';
 import { SentryConfigEditor } from './editors/SentryConfigEditor';
 import { SentryQueryEditor } from './editors/SentryQueryEditor';
 import { SentryVariableEditor } from './editors/SentryVariableEditor';
 import type { SentryConfig, SentrySecureConfig, SentryQuery } from './types';
+import pluginJson from './plugin.json';
 
 export const plugin = new DataSourcePlugin<SentryDataSource, SentryQuery, SentryConfig, SentrySecureConfig>(SentryDataSource)
   .setConfigEditor(SentryConfigEditor)
   .setQueryEditor(SentryQueryEditor)
   .setVariableQueryEditor(SentryVariableEditor);
+
+// Track dashboard loads to RudderStack
+getAppEvents().subscribe<DashboardLoadedEvent<SentryQuery>>(
+  DashboardLoadedEvent,
+  ({ payload: { dashboardId, orgId, grafanaVersion, queries } }) => {
+    const sentryQueries = queries["grafana-sentry-datasource"];
+
+    if (!sentryQueries?.length) {
+      return;
+    };
+
+    trackSentryDashboardLoaded({
+      sentry_plugin_version: pluginJson.info.version,
+      dashboardId: dashboardId,
+      grafanaVersion: grafanaVersion,
+      orgId: orgId,
+      ...analyzeQueries(sentryQueries),
+    });
+  }
+);
