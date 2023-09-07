85935b6c83ba9d65458ae45d42a070a2f1d6e05d
Andreas Christou <andreas.christou@grafana.com>
2023-09-07 17:44:08 +0100

    Fix e2e

--- cypress/integration/02_variable_editor.spec.ts
+++ cypress/integration/02_variable_editor.spec.ts
@@ -28,11 +28,11 @@ e2e.scenario({
       })
       .then((ds) => {
         e2e.flows.addDashboard().then(() => {
-          openDashboardSettings('Variables');
+          e2e.components.PageToolbar.item('Dashboard settings').click();
+          e2e.components.Tab.title('Variables').click();
           e2e.pages.Dashboard.Settings.Variables.List.addVariableCTAV2().click();
-          e2e.pages.Dashboard.Settings.Variables.Edit.General.generalNameInput().clear().type('variable1');
-          cy.wait(6 * 1000); // When clearing the variable name, the validation popup comes and hides the datasource picker. so wait sometime till the popup closes.
-          selectDropdown(e2e.pages.Dashboard.Settings.Variables.Edit.QueryVariable.queryOptionsDataSourceSelect(), ds.config.name);
+          e2e.pages.Dashboard.Settings.Variables.Edit.General.generalNameInputV2().clear().type('variable1');
+          e2e.components.DataSourcePicker.inputV2().type(`${ds.config.name}{enter}`);
           // Get list of projects
           selectDropdown(e2eSelectors.VariablesEditor.QueryType.container.ariaLabel(), 'Projects');
           cy.wait(2 * 1000);
@@ -44,7 +44,10 @@ e2e.scenario({
           selectDropdown(e2eSelectors.VariablesEditor.Project.container.ariaLabel(), SENTRY_E2E_PROJECT_NAME);
           cy.wait(2 * 1000);
           variableEditorPreviewValuesCheck([SENTRY_E2E_ENVIRONMENT_NAME], [SENTRY_E2E_NODE_ONLY_ENVIRONMENT_NAME]);
-          selectDropdown(e2eSelectors.VariablesEditor.Project.container.ariaLabel(), SENTRY_E2E_PRODUCTION_PROJECT_NAME);
+          selectDropdown(
+            e2eSelectors.VariablesEditor.Project.container.ariaLabel(),
+            SENTRY_E2E_PRODUCTION_PROJECT_NAME
+          );
           cy.wait(2 * 1000);
           variableEditorPreviewValuesCheck([SENTRY_E2E_ENVIRONMENT_NAME, SENTRY_E2E_NODE_ONLY_ENVIRONMENT_NAME]);
         });
