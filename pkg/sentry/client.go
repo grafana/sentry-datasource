4213d29f6cd4b71ff7c909a4a19184dfb5ddbf95
Oleg Yevik <olegyevik@canva.com>
2023-10-24 14:29:02 +1300

    Handle rate limiter errors

--- pkg/sentry/client.go
+++ pkg/sentry/client.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"time"	
 
 	"github.com/grafana/grafana-plugin-sdk-go/build"
 	"github.com/grafana/sentry-datasource/pkg/mocker"
@@ -12,6 +14,8 @@ import (
 type doer interface {
 	Do(*http.Request) (*http.Response, error)
 }
+
+const maxRetryAttempts = 3
 
 // HTTP creates an HTTP client with a 'Do' method. It automatically injects
 // the provided api token into every request with an `Authorization: Bearer AuthToken` header
@@ -37,12 +41,41 @@ func NewHTTPClient(d doer, pluginId string, b BuildInfoProvider, authToken strin
 
 // Do attaches the sentry authentication header and the User-Agent header to
 // the request and passes it to the injected http Doer
-func (a HTTPClient) Do(req *http.Request) (*http.Response, error) {
+func (a HTTPClient) Do(req *http.Request, rateLimitsRetryOnFailure bool) (*http.Response, error) {
 	req.Header.Set("User-Agent", fmt.Sprintf("%s/%s", a.pluginId, a.version))
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.authToken))
 	_, E2E_GRAFANA_VERSION_PRESENT := os.LookupEnv("E2E_GRAFANA_VERSION")
 	if E2E_GRAFANA_VERSION_PRESENT {
 		return mocker.Mock(req)
 	}
-	return a.doer.Do(req)
+	retryCount := 0
+	for {
+		resp, err := a.doer.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode == http.StatusTooManyRequests && rateLimitsRetryOnFailure && retryCount < maxRetryAttempts {
+			// Check for "X-Sentry-Rate-Limit-Reset" header
+			resetTimeStr := resp.Header.Get("X-Sentry-Rate-Limit-Reset")
+			if resetTimeStr != "" {
+				resetTime, err := strconv.ParseInt(resetTimeStr, 10, 64)
+				if err != nil {
+					return nil, err
+				}
+				currentTime := time.Now().Unix()
+
+				if resetTime > currentTime {
+					// Sleep until the reset time and then retry
+					sleepDuration := time.Duration(resetTime - currentTime)
+					time.Sleep(sleepDuration)
+				}
+			}
+
+			retryCount++
+			continue
+		}
+
+		return resp, nil
+	}
 }
